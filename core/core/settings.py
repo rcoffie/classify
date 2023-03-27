7d31020977486d196a09c45078cd082b218e7c81
Robert Coffie <rcoffie22@yahoo.com>
2023-03-27 10:33:58 +0000

    configuring postgresql and updating requirments.txt file

--- core/core/settings.py
+++ core/core/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'classify',
+        'USER': 'qgis',
+        'PASSWORD': 'nana',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
