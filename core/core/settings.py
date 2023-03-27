f0ef0047e117dea5fc3ae3731859842603a8cc0c
Robert Coffie <rcoffie22@yahoo.com>
2023-03-27 11:03:18 +0000

    adding django decouple

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5!#k!1qw)ryx=j4qxl-e8uz7vyl9*ly1%@4hza=ww&mr9)uhh9'
+# SECRET_KEY = 'django-insecure-5!#k!1qw)ryx=j4qxl-e8uz7vyl9*ly1%@4hza=ww&mr9)uhh9'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -83,10 +86,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'classify',
-        'USER': 'qgis',
-        'PASSWORD': 'nana',
-        'HOST': 'localhost',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '',
     }
 }
