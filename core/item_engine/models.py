8828f76d103ac9fde4737e60f3d36c836d6d15a3
Robert Coffie <rcoffie22@yahoo.com>
2023-03-28 10:13:11 +0000

    adding itmes model

--- core/item_engine/models.py
+++ core/item_engine/models.py
@@ -17,3 +17,24 @@ class Category(models.Model):
       
     def __str__(self):
         return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField() 
+    is_sold = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    # In future set image path and default image
+    Image = models.ImageField(null=True,blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [models.Index(fields=['name','price','category'])]
+
+
+    def __str__(self):
+        return self.name 
+
