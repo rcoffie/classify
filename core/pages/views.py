9476ac0dec8af0a5bd3895d154e44f8fd5db9d91
Robert Coffie <rcoffie22@yahoo.com>
2023-03-28 11:53:21 +0000

    refactoring pages_app urls and view

--- core/pages/views.py
+++ core/pages/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
 
-    return render(request, 'pages/home.html')
+def home(request):
+    return render(request, "pages/home.html")
+
 
 def contact(request):
-
-    return render(request, 'pages/contact.html')
+    return render(request, "pages/contact.html")
