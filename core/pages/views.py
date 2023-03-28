065463490e33dfbdad176e74bf110be3f8400f66
Robert Coffie <rcoffie22@yahoo.com>
2023-03-28 14:28:33 +0000

    displaying list items on home page

--- core/pages/views.py
+++ core/pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
+from item_engine.models import Category, Item
 # Create your views here.
 
 
 def home(request):
-    return render(request, "pages/home.html")
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    context = {
+        'items':items, 
+        'categories':categories,
+        }
+    return render(request, "pages/home.html",context)
 
 
 def contact(request):
