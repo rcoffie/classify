061b75781bf851eee53551247ce6342902674e61
Robert Coffie <rcoffie22@yahoo.com>
2023-03-29 08:16:59 +0000

    finalizing homepage template

--- core/item_engine/views.py
+++ core/item_engine/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from . models import Category, Item
 # Create your views here.
+
+
+def item_detail(request, id):
+    item = get_object_or_404(Item, id=id)
+    related_items = Item.objects.filter(category=item.category,)
+    context = {'item':item, 'related_items':related_items,}
+    return render(request, 'item_engine/item-detail.html',context)
