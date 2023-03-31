75995fac851a753d4ec0d1fa36f6bd7511a332de
Robert Coffie <rcoffie22@yahoo.com>
2023-03-31 09:07:32 +0000

    working on add item_form

--- core/item_engine/views.py
+++ core/item_engine/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from . models import Category, Item
+from .forms import ItemForm
 # Create your views here.
 
 
@@ -8,3 +9,18 @@ def item_detail(request, id):
     related_items = Item.objects.filter(category=item.category,).exclude(id=id)[0:3]
     context = {'item':item, 'related_items':related_items,}
     return render(request, 'item_engine/item-detail.html',context)
+
+
+def add_item(request):
+    form = ItemForm()
+    if request.method == 'POST':
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+            return redirect('item_detail', id=item.id)
+        else:
+            form = ItemForm()
+    context = {'form':form}
+    return render(request, 'item_engine/add-item-form.html',context)
