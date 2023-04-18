bf132364492aedcd6c330f446a1580965aeb6445
Robert Coffie <rcoffie22@yahoo.com>
2023-04-18 09:31:27 +0000

    fixing file handler not found in logs error

--- core/core/settings.py
+++ core/core/settings.py
@@ -177,7 +177,7 @@ LOGGING = {
         'file': {
             'level': 'WARNING',
             'class': 'logging.FileHandler',
-            'filename': '../warning.log',
+            'filename': '../logs/warning.log',
         },
     },
     'loggers': {
