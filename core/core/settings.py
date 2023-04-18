0cfe1602fef4e0b2bae52f8939ee9d0b1b3311a9
Robert Coffie <rcoffie22@yahoo.com>
2023-04-18 09:28:11 +0000

    updating master

--- core/core/settings.py
+++ core/core/settings.py
@@ -177,7 +177,7 @@ LOGGING = {
         'file': {
             'level': 'WARNING',
             'class': 'logging.FileHandler',
-            'filename': '../logs/warning.log',
+            'filename': '../warning.log',
         },
     },
     'loggers': {
